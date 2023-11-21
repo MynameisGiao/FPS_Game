cdea4380c055f978d8453306bfefb58a55ff32ca
MynameisGiao <2004phuonggiao@gmail.com>
2023-11-21 14:10:32 +0700

    mission

--- Assets/Plugins/ART 3D/SurrounDead/Documentation/README.txt
+++
@@ -1,7 +0,0 @@
-For the demo scenes to work properly please make sure you follow the steps below:
-1. Set Rendering Path to Deferred: Edit-> Project Settings -> Graphics -> Set Rendering Path to Deferred
-2. Set Rendering Color SpaceSet: Edit-> Project Settings -> Player -> Set Rendering Color Space To Gamma
-3. Download and import Unity's Standard Assets Environment Package via Asset Store -> Standard Assets -> Environment (for water)
-4. Download Post Processing Stack v2 from Window -> Package Manager -> Post Processing
-5. If you get lots of errors after importing go to Window -> Package Manger-> Package Manager UI and update it.
-6. Restart Unity to take effect.
---
+++ Assets/Scrips/DataTable/Base/ConfigManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigManager : BYSingletonMono<ConfigManager>
+{
+    //public ConfigEnemy configEnemy;
+    //public ConfigMission configMission;
+    //public ConfigWave configWave;
+    //public ConfigShop configShop;
+    //public ConfigUnit configUnit;
+    //public ConfigUnitLevel configUnitLevel;
+    //// Start is called before the first frame update
+    //public void InitConfig(Action callback)
+    //{
+    //    StartCoroutine(ProgressLoadConfig(callback));
+    //}
+    //IEnumerator ProgressLoadConfig(Action callback)
+    //{
+    //    configEnemy = Resources.Load("Config/ConfigEnemy", typeof(ScriptableObject)) as ConfigEnemy;
+    //    yield return new WaitUntil(() => configEnemy != null);
+
+    //    configMission = Resources.Load("Config/ConfigMission", typeof(ScriptableObject)) as ConfigMission;
+    //    yield return new WaitUntil(() => configMission != null);
+
+    //    configWave = Resources.Load("Config/ConfigWave", typeof(ScriptableObject)) as ConfigWave;
+    //    yield return new WaitUntil(() => configWave != null);
+
+    //    configShop = Resources.Load("Config/ConfigShop", typeof(ScriptableObject)) as ConfigShop;
+    //    yield return new WaitUntil(() => configShop != null);
+
+    //    configUnit = Resources.Load("Config/ConfigUnit", typeof(ScriptableObject)) as ConfigUnit;
+    //    yield return new WaitUntil(() => configUnit != null);
+
+    //    configUnitLevel = Resources.Load("Config/ConfigUnitLevel", typeof(ScriptableObject)) as ConfigUnitLevel;
+    //    yield return new WaitUntil(() => configUnitLevel != null);
+
+    //    if (callback != null)
+    //        callback();
+
+    //    //callback?.Invoke();
+    //}
+}
